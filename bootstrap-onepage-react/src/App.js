70729ce64464d4cba19b9f84f5986cd68934b6ca
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-04-17 15:13:24 +0200

    Ran CRA, added reactstrap

---
+++ bootstrap-onepage-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button color="primary">Test</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
