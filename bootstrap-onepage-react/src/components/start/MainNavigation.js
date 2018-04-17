525d89d52d5243ed54aecf2c9529e969393af872
Felix Pojtinger <felix@pojtinger.com>
2018-04-17 16:38:13 +0200

    Added main nav and basic content

---
+++ bootstrap-onepage-react/src/components/start/MainNavigation.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { ContactModal } from "./ContactModal";
+
+export class MainNavigation extends Component {
+  state = {
+    navbarIsOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      navbarIsOpen: !this.state.navbarIsOpen
+    });
+  };
+  render() {
+    const { toggle } = this;
+    const { title } = this.props;
+    const { navbarIsOpen } = this.state;
+
+    return (
+      <Navbar color="primary" dark expand="sm" className="fixed-top">
+        <NavbarBrand href="/">{title}</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={navbarIsOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#who-we-are">Who we are</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#what-we-do">What we do</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#our-customers">Our customers</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#showcase">Showcase</NavLink>
+            </NavItem>
+            <NavItem>
+              <ContactModal buttonLabel="Contact us" />
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
---
+++ bootstrap-onepage-react/src/components/start/Start.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StartLayout } from "../../layouts/StartLayout";
+import { Section } from "../global/Section";
+
+export const Start = () => (
+  <StartLayout>
+    <Section title="Who we are" link="who-we-are">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis,
+      alias, animi, hic nam distinctio quas veniam temporibus quis ut sit
+      eligendi! Accusamus ullam totam veniam, cupiditate maiores rerum rem vero
+      vitae! Accusantium, ad at praesentium laborum odio iste vel. Alias
+      inventore sint error totam in, voluptate nemo voluptatem? Dicta, at.
+    </Section>
+    {/* Header */}
+    {/* StartSection */}
+    {/* TextBoxes */}
+    {/* Slider */}
+    {/* Modal */}
+    {/* Footer */}
+  </StartLayout>
+);
